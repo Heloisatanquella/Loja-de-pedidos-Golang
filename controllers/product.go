b7547e460973b70df38123d23057de1f7b038e4a
heloisatanquella <heloisacristovao02@gmail.com>
2024-12-17 14:53:14 -0300

    feat: func Produto

--- controllers/product.go
+++ controllers/product.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"loja-de-pedidos-golang/entities"
 	"strings"
 )
@@ -15,4 +16,63 @@ func BuscarProduto(nomeProduto string, produtosEmEstoque []entities.Produto) *en
 	}
 
 	return nil
+}
+
+func Produto(produtosEmEstoque *[]entities.Produto) {
+	var nomeProduto string
+	var descricao string
+	var preco float64
+	var quantidade uint
+
+	for {
+		fmt.Printf("\nDigite o nome do produto: ")
+		fmt.Scanln(&nomeProduto)
+
+		if BuscarProduto(nomeProduto, *produtosEmEstoque) != nil {
+			fmt.Println("\nProduto já existente. Tente novamente.")
+		} else {
+			break
+		}
+	}
+
+	fmt.Printf("\nInsira uma descrição para o produto: ")
+	fmt.Scanln(&descricao)
+
+	for {
+		fmt.Printf("\nInsira o preço do produto (00.00): R$ ")
+		_, err := fmt.Scanln(&preco)
+
+		if err != nil || preco <= 0 {
+			fmt.Println("\nO valor inserido não é válido! Tente novamente.")
+			var limpar string
+			fmt.Scanln(&limpar)
+			continue
+		}
+		break
+	}
+
+	for {
+		fmt.Printf("\nInsira a quantidade em estoque: ")
+		_, err := fmt.Scanln(&quantidade)
+
+		if err != nil || quantidade <= 0 {
+			fmt.Println("\nEntrada inválida. Por favor, insira um número inteiro positivo.")
+			var limpar string
+			fmt.Scanln(&limpar)
+			continue
+		}
+		break
+	}
+
+	novoProduto := entities.Produto{
+		Nome:       nomeProduto,
+		Descricao:  descricao,
+		Preco:      preco,
+		Quantidade: quantidade,
+	}
+
+	*produtosEmEstoque = append(*produtosEmEstoque, novoProduto)
+
+	fmt.Println("\nDados do produto cadastrado:")
+	novoProduto.DadosProduto()
 }
--- entities/product.go
+++ entities/product.go
@@ -9,6 +9,6 @@ type Produto struct {
 	Quantidade uint
 }
 
-func DadosProduto(p Produto) {
+func (p Produto) DadosProduto() {
 	fmt.Printf("\nNome do produto: %s; Dsecrição: %s; Preço unitário: %.2f; Quantidade disponível: %d;\n", p.Nome, p.Descricao, p.Preco, p.Quantidade)
 }
